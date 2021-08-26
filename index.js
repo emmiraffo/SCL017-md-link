851e6b2b9bed9d31cc53a0fe2e97e7419873c360
Emmi Raffo <emmyraffoc@gmail.com>
2021-08-25 20:52:08 -0400

    expresion regular

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+function main(resolve) {
+ //1. Leer un archivo del sistema de archivos (Fs)
+ fs.readFile('README.md', 'utf8', function(err, data) {
+   //2. Ejecutar una expresion regular sobre el contenido del archivo 
+  var links = data.match(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi)
+   //3. Retornar el resultado de la expresion regular
+  resolve(links)
+ });
+
+
+
+
+
+}
+
+
 module.exports = () => {
-  console.log("esto es un test")
+  return new Promise(main)
 };
